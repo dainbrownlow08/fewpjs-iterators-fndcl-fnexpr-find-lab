817537f0a834a847955b381cdd451bc88d976e74
dainbrownlow08 <dain.brownlow@outlook.com>
2021-01-15 19:56:19 -0800

    commit tree 8a9cf18c6cfcaf489067974bfe736ab213a4fc7a

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let o = arr.find(function(g){ return g.result === "W"})
+  return (o ? o.year : undefined)
+}
